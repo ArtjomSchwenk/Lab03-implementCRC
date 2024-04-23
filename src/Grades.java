8afc92c20ee03cb851f3275ed1a578d3257b4e3b
MrCake4 <winstonjreichelt@gmail.com>
2024-04-23 16:11:58 +0200

    added comment

--- src/Grades.java
+++ src/Grades.java
@@ -7,7 +7,7 @@ public class Grades{
     public  Map<Integer, String> grades = new HashMap<>();
     public void createGradeReport(int id, String grade){
 
-        grades.put(id, grade);
+        grades.put(id, grade);      // TODO: the grade is stored as a string, but it should be stored as a double
     }
     public String getGradeReport(int id){
         return grades.get(id);
--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,6 @@ public class Main {
         Student s1 = new Student("Dieter", 12345);
         Student s2 = new Student("Hans", 54321);
 
-        /*
-        System.out.println("src.Student-Name: " + s1.getName());
-        System.out.println("ID: "+ s1.getId());*/
-
         Grades gradeReport = new Grades();
         gradeReport.createGradeReport(12345, "Info: 1.3");
 
