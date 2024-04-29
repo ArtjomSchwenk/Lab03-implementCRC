40d2487259df1ccca0183629cda99d2892955bea
Artjom <s0577519@htw-berlin.de>
2024-04-29 19:04:59 +0200

    A student can now view their grade through the CLI. If there are no grade, an appropriate response is given

--- src/Cli.java
+++ src/Cli.java
@@ -4,7 +4,7 @@ public class Cli {
     public int option;
     public Student student;
 
-    public int getInput(){
+    public UserInput getInput(){
         java.util.Scanner scanner = new java.util.Scanner(System.in);
 
         System.out.println("==========\nLSD RUNNING\n==========\nWhats your name?");
@@ -13,7 +13,11 @@ public class Cli {
         System.out.println("Your id?");
         int id = scanner.nextInt();
 
+        Student student = new Student(name, id);
+
         System.out.println("What do you want to do?\n1.Check notes\n2.Exit");
-        return scanner.nextInt();
+        int option = scanner.nextInt();
+
+        return new UserInput(option, student);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,18 @@ public class Main {
         //UnitTests tests = new UnitTests();
         //tests.runAllTests();
         Cli cli = new Cli();
-        cli.getInput();
+        Grades grades = new Grades();
+
+        UserInput userInput = cli.getInput();
+        int option = userInput.option;
+        Student student = userInput.student;
+        int id = student.getId();
+
+        grades.createGradeReport(id, "Info", 2.3);
+
+        if(option == 1){
+            System.out.println(grades.getGradeReport(id));
+        }
+
     }
 }
